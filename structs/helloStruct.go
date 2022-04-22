f59741d81e7834f46ba387a1913ddb10a7ef3688
resqiar <resqiageng@gmail.com>
2022-04-22 21:39:28 +0700

    chore: delete unnecessary code

--- structs/helloStruct.go
+++
@@ -1,6 +0,0 @@
-package structs
-
-type PostHello struct {
-	Id    string `json:"id" binding:"required"`
-	Title string `json:"title" binding:"required"`
-}
--- users/user.input.go
+++
@@ -1,8 +0,0 @@
-package users
-
-type UserInput struct {
-	Username string `json:"username" binding:"required"`
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required"`
-	Bio      string `json:"desc"`
-}
